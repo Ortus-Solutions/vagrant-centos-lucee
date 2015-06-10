597c3a47bf3a1b3193b3a6b522966852cfb46ef0
Brad Wood <brad@bradwood.com>
2015-06-10 11:52:34 -0700

    Updated commits from pulls upstream

--- bash-scripts/step-4-install-lucee.sh
+++ bash-scripts/step-4-install-lucee.sh
@@ -5,17 +5,35 @@ echo " "
 echo "BEGIN setting up Lucee"
 
 # install lucee
-echo "... Copying the Lucee installer into the VM ..."
-cp /vagrant/bin/lucee-4.5.1.000-pl0-linux-x64-installer.run /root
-cp /vagrant/configs/lucee-options.txt /root
-chmod +x /root/lucee-4.5.1.000-pl0-linux-x64-installer.run
+if [ ! -d "/opt/lucee" ]; then
 
-echo "... Installing Lucee ..."
-/root/lucee-4.5.1.000-pl0-linux-x64-installer.run --mode unattended --optionfile /root/lucee-options.txt
+	
+	# Don't download if we've already got it locally
+	if [ -f "/vagrant/artifacts/lucee-4.5.1.000-pl0-linux-x64-installer.run" ]; then
+		echo "... Copying Lucee installer ..."
+		sudo cp /vagrant/artifacts/lucee-4.5.1.000-pl0-linux-x64-installer.run /root
+	else
+		echo "... Downloading the Lucee installer, standby ..."
+		wget -O /root/lucee-4.5.1.000-pl0-linux-x64-installer.run http://railo.viviotech.net/downloader.cfm/id/133/file/lucee-4.5.1.000-pl0-linux-x64-installer.run &> /dev/null
+		sudo cp /root/lucee-4.5.1.000-pl0-linux-x64-installer.run /vagrant/artifacts
+
+	fi
+	
+	
+	
+	echo "... Installing Lucee ..."
+	chmod +x /root/lucee-4.5.1.000-pl0-linux-x64-installer.run
+	cp /vagrant/configs/lucee-options.txt /root
+	/root/lucee-4.5.1.000-pl0-linux-x64-installer.run --mode unattended --optionfile /root/lucee-options.txt
+fi
+
+
 
 echo "... Copying the Lucee config files into place ..."
-#sudo cp /vagrant/configs/setenv.sh /opt/lucee/tomcat/bin
-cp /vagrant/configs/lucee-server.xml /opt/lucee/lib/lucee-server/context
+sudo cp /vagrant/configs/setenv.sh /opt/lucee/tomcat/bin
+#sudo cp /vagrant/configs/lucee-server.xml /opt/lucee/lib/lucee-server/context
+#sudo cp /vagrant/configs/server.xml /opt/lucee/tomcat/conf/server.xml
+
 
 echo "... Restarting Lucee ..."
 service lucee_ctl restart > /dev/null
