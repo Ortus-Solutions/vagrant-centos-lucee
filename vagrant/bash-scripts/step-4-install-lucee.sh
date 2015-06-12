107d4c3b8fa0d3c0c9e6fe387aada869a2e5448e
Luis Majano <lmajano@gmail.com>
2015-06-12 10:54:26 -0700

    ability to patch the lucee server on demand

--- vagrant/bash-scripts/step-4-install-lucee.sh
+++ vagrant/bash-scripts/step-4-install-lucee.sh
@@ -4,39 +4,66 @@ echo "================= START STEP-4-INSTALL-LUCEE.SH $(date +"%r") ============
 echo " "
 echo "BEGIN setting up Lucee"
 
-# install lucee
-if [ ! -d "/opt/lucee" ]; then
+##############################################################################################
+## Lucee Installation
+##############################################################################################
+LUCEE_VERSION="4.5.1.000"
 
+if [ ! -d "/opt/lucee" ]; then
 	
 	# Don't download if we've already got it locally
-	if [ -f "/vagrant/artifacts/lucee-4.5.1.000-pl0-linux-x64-installer.run" ]; then
-		echo "... Copying Lucee installer ..."
-		sudo cp /vagrant/artifacts/lucee-4.5.1.000-pl0-linux-x64-installer.run /root
-	else
+	if [ ! -f "/vagrant/artifacts/lucee-$LUCEE_VERSION-pl0-linux-x64-installer.run" ]; then
 		echo "... Downloading the Lucee installer, standby ..."
-		wget -O /root/lucee-4.5.1.000-pl0-linux-x64-installer.run http://railo.viviotech.net/downloader.cfm/id/133/file/lucee-4.5.1.000-pl0-linux-x64-installer.run &> /dev/null
-		sudo cp /root/lucee-4.5.1.000-pl0-linux-x64-installer.run /vagrant/artifacts
-
+		wget -O /vagrant/artifacts/lucee-$LUCEE_VERSION-pl0-linux-x64-installer.run http://downloads.ortussolutions.com/lucee/lucee/$LUCEE_VERSION/lucee-$LUCEE_VERSION-pl0-linux-x64-installer.run  &> /dev/null
 	fi
-	
-	
+
+	echo "... Copying Lucee installer ..."
+	sudo cp /vagrant/artifacts/lucee-$LUCEE_VERSION-pl0-linux-x64-installer.run /root
 	
 	echo "... Installing Lucee ..."
-	chmod +x /root/lucee-4.5.1.000-pl0-linux-x64-installer.run
+	chmod +x /root/lucee-$LUCEE_VERSION-pl0-linux-x64-installer.run
 	cp /vagrant/configs/lucee-options.txt /root
-	/root/lucee-4.5.1.000-pl0-linux-x64-installer.run --mode unattended --optionfile /root/lucee-options.txt
+	/root/lucee-$LUCEE_VERSION-pl0-linux-x64-installer.run --mode unattended --optionfile /root/lucee-options.txt
+else
+	echo "Lucee setup already, skipping"
 fi
 
+##############################################################################################
+## Patch lucee
+###############################################################################################
+PATCH_VERSION="4.5.1.020"
+PATCH_FILE="$PATCH_VERSION.lco"
+PATCH_FORCE=1
 
+echo "BEGIN Lucee Patching: $PATCH_FILE";
 
+# Don't patch if already patched
+if [ ! -f /opt/lucee/lib/lucee-server/patches/$PATCH_FILE ] || [ $PATCH_FORCE -eq 1 ]; then
+	# Don't download if we've already got it locally
+	if [ ! -f "/vagrant/artifacts/lucee-$PATCH_FILE" ]; then
+		echo "... Downloading the Lucee patch v$PATCH_VERSION, standby ..."
+		wget -O /vagrant/artifacts/lucee-$PATCH_FILE http://downloads.ortussolutions.com/lucee/lucee/$PATCH_VERSION/lucee-$PATCH_FILE  &> /dev/null
+	fi
+
+	echo "... Copying Lucee Patch ..."
+	sudo cp /vagrant/artifacts/lucee-$PATCH_FILE /opt/lucee/lib/lucee-server/patches/$PATCH_FILE
+else 
+	echo "Lucee patched already, skipping"
+fi
+
+##############################################################################################
+## Lucee Configuration
+##############################################################################################
 echo "... Copying the Lucee config files into place ..."
 sudo cp /vagrant/configs/setenv.sh /opt/lucee/tomcat/bin
 #sudo cp /vagrant/configs/lucee-server.xml /opt/lucee/lib/lucee-server/context
 #sudo cp /vagrant/configs/server.xml /opt/lucee/tomcat/conf/server.xml
 
-
+##############################################################################################
+## Restarting
+##############################################################################################
 echo "... Restarting Lucee ..."
-service lucee_ctl restart > /dev/null
+#service lucee_ctl restart > /dev/null
 
 echo "... END setting up Lucee."
 echo " "
