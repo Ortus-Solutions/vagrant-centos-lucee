57e6b601a3253b228fea281ed421536ca96cd50a
Luis Majano <lmajano@gmail.com>
2015-07-30 17:11:23 -0500

    remove webmin

--- vagrant/provisioners/install-utilities.sh
+++ vagrant/provisioners/install-utilities.sh
@@ -16,11 +16,11 @@ if [ ! -f /var/log/utils_installed ]; then
 	yum install unzip -y &>> /vagrant/log/install.txt
 	yum install net-tools -y &>> /vagrant/log/install.txt
 
-	echo "... Installing WebMin ..."
-	wget -O /root/jcameron-key.asc http://www.webmin.com/jcameron-key.asc &>> /vagrant/log/install.txt
-	rpm --import /root/jcameron-key.asc
-	cp /vagrant/configs/webmin.repo /etc/yum.repos.d/
-	yum install webmin -y &>> /vagrant/log/install.txt
+	#echo "... Installing WebMin ..."
+	#wget -O /root/jcameron-key.asc http://www.webmin.com/jcameron-key.asc &>> /vagrant/log/install.txt
+	#rpm --import /root/jcameron-key.asc
+	#cp /vagrant/configs/webmin.repo /etc/yum.repos.d/
+	#yum install webmin -y &>> /vagrant/log/install.txt
 
 	touch /var/log/utils_installed
 fi
